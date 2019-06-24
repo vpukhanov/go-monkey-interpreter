a37ba42285935360974c2c2356e651ef98683fac
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2019-06-24 22:40:42 +0300

    adds builtin 'len' function

--- object/object.go
+++ object/object.go
@@ -17,12 +17,15 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type BuiltinFunction func(args ...Object) Object
 
 type Integer struct {
 	Value int64
@@ -88,3 +91,10 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
